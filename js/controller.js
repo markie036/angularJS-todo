6773b9ea54dcbfb4da10179b066effc130fe4668
Mark <mharrisondesign@mail.com>
2017-07-15 10:33:54 +0100

    Add js

--- js/controller.js
+++ js/controller.js
@@ -1,34 +1,35 @@
 angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
-        $scope.title = "Welcome To Angular Todo!";
+        $scope.title = "Welcome To Angular Todo!"
     })
-    .controller('RegisterController', function($scope, $location, UserAPIService) {
- 
+    .controller('RegisterController', function($scope, UserAPIService) {
         $scope.registrationUser = {};
         var URL = "https://morning-castle-91468.herokuapp.com/";
 
         //Code for when the user has logged in
         $scope.login = function() {
-            UserAPIService.registerUser(URL + "accounts/api-token-auth/", $scope.data).then(function(results) {
+            UserAPIService.callAPI(URL + "accounts/api-token-auth/", $scope.data).then(function(results) {
                 $scope.token = results.data.token;
                 console.log($scope.token);
             }).catch(function(err) {
-                console.log(err.data);
+                console.log(err);
             });
         }
         //Login code ends
- 
+
         $scope.submitForm = function() {
             if ($scope.registrationForm.$valid) {
                 $scope.registrationUser.username = $scope.user.username;
                 $scope.registrationUser.password = $scope.user.password;
- 		//registerUser been changed to callAPI as user has now been registered
-                UserAPIService.registerUser(URL + "accounts/register/", $scope.registrationUser).then(function(results) {
+
+        //registerUser been changed to callAPI as user has now been registered
+                UserAPIService.callAPI(URL + "accounts/register/", $scope.registrationUser).then(function(results) {
                     $scope.data = results.data;
                     alert("You have successfully registered to Angular Todo");
+                    $scope.login();
                 }).catch(function(err) {
-                    alert("Oops! Something went wrong!");
                     console.log(err);
+                    alert("Registration failed, please try again with another username.");
                 });
             }
         };
