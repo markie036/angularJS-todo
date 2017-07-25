e305d17fcfdc6d4bca0c370929f8de0bc0b0abfe
Mark <mharrisondesign@mail.com>
2017-07-25 22:01:30 +0100

    Add controller.js

--- js/controller.js
+++ js/controller.js
@@ -42,3 +42,25 @@ angular.module('RouteControllers', [])
             }
         };
     });
+
+    .controller('TodoController', function($scope, $location, TodoAPIService, store) {
+        var URL = "https://morning-castle-91468.herokuapp.com/";
+ 
+        $scope.authToken = store.get('authToken');
+        $scope.username = store.get('username');
+ 
+        $scope.todos = [];
+ 
+        $scope.submitForm = function() {
+            if ($scope.todoForm.$valid) {
+                $scope.todo.username = $scope.username;
+                $scope.todos.push($scope.todo);
+ 
+                TodoAPIService.createTodo(URL + "todo/", $scope.todo, $scope.authToken).then(function(results) {
+                    console.log(results)
+                }).catch(function(err) {
+                    console.log(err)
+                });
+            }
+        }
+    });
