486a02b354ce18a2ad4f5664df1eae4655a0a543
Mark <mharrisondesign@mail.com>
2017-07-26 21:27:36 +0100

    Add app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ angular.module('TodoApp', ['ngRoute', 'RouteControllers', 'UserService', 'angula
 
 angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);  // Enable href routing without hashes
- 
+    
     $routeProvider.when('/', {
         templateUrl: 'templates/home.html',
         controller: 'HomeController'
