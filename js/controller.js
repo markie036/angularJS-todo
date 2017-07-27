264b4e0b684540d9847865c70dd5d1c1b93fcbf1
Mark <mharrisondesign@mail.com>
2017-07-27 21:33:50 +0100

    Add controller.js

--- js/controller.js
+++ js/controller.js
@@ -79,12 +79,6 @@ angular.module('RouteControllers', [])
      }).controller('EditTodoController', function($scope, $location, $routeParams, TodoAPIService, store) {
         var id = $routeParams.id;
         var URL = "https://morning-castle-91468.herokuapp.com/";
-
-        TodoAPIService.getTodos(URL + "todo/" + id, $scope.username, store.get('authToken')).then(function(results) {
-            $scope.todo = results.data;
-        }).catch(function(err) {
-            console.log(err);
-        });
  
         $scope.submitForm = function() {
             if ($scope.todoForm.$valid) {
@@ -97,4 +91,5 @@ angular.module('RouteControllers', [])
                 })
             }
         }
+    });
     });
